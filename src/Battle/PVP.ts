4824b6190f17b0e8e22e27f8d61c2d01b93366b6
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-05-08 17:00:08 -0300

    feito

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,7 +1,21 @@
+import Character from '../Character';
 import Battle from './Battle';
 
 class PVP extends Battle {
+  protected playerDois: Character;
+
+  constructor(protected pUm: Character, pDois: Character) {
+    super(pUm);
+    this.playerDois = pDois;
+  }
   
+  fight(): number {
+    while (this.player.lifePoints === -1 || this.playerDois.lifePoints === -1) {
+      this.player.attack(this.playerDois);
+      this.playerDois.attack(this.player);  
+    }
+    return super.fight();
+  }
 }
 
 export default PVP;
